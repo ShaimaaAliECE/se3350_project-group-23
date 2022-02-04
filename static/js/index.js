75e03f4dd34c6d808b81812fd071a0422df00629
justinbeaudry10 <jbeaudr5@uwo.ca>
2022-02-04 13:29:19 -0500

    Rearranged start, quit btns and message

--- static/js/index.js
+++ static/js/index.js
@@ -1,99 +1,99 @@
 class BinaryTreeNode {
-    constructor(key, value = key, parent = null, left = null, right = null) {
-        this.key = key;
-        this.value = value;
-        this.parent = parent;
-        this.left = left;
-        this.right = right;
-    }
+  constructor(key, value = key, parent = null, left = null, right = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = left;
+    this.right = right;
+  }
 
-    get isLeaf() {
-        return this.left === null && this.right === null;
-    }
+  get isLeaf() {
+    return this.left === null && this.right === null;
+  }
 
-    get hasChildren() {
-        return !this.isLeaf;
-    }
+  get hasChildren() {
+    return !this.isLeaf;
+  }
 
-    get getSortedValue() {
-      let copy = [...this.value];
-      copy.sort((a, b) => {
-        return a - b;
-      });
-      return copy;
-    }
+  get getSortedValue() {
+    let copy = [...this.value];
+    copy.sort((a, b) => {
+      return a - b;
+    });
+    return copy;
+  }
 }
 
 class BinaryTree {
-    constructor(key, value = key) {
-        this.root = new BinaryTreeNode(key, value);
-    }
+  constructor(key, value = key) {
+    this.root = new BinaryTreeNode(key, value);
+  }
 
-    *inOrderTraversal(node = this.root) {
-        if (node.left) yield* this.inOrderTraversal(node.left);
-        yield node;
-        if (node.right) yield* this.inOrderTraversal(node.right);
-    }
+  *inOrderTraversal(node = this.root) {
+    if (node.left) yield* this.inOrderTraversal(node.left);
+    yield node;
+    if (node.right) yield* this.inOrderTraversal(node.right);
+  }
 
-    *postOrderTraversal(node = this.root) {
-        if (node.left) yield* this.postOrderTraversal(node.left);
-        if (node.right) yield* this.postOrderTraversal(node.right);
-        yield node;
-    }
+  *postOrderTraversal(node = this.root) {
+    if (node.left) yield* this.postOrderTraversal(node.left);
+    if (node.right) yield* this.postOrderTraversal(node.right);
+    yield node;
+  }
 
-    *preOrderTraversal(node = this.root) {
-        yield node;
-        if (node.left) yield* this.preOrderTraversal(node.left);
-        if (node.right) yield* this.preOrderTraversal(node.right);
-    }
-    
-    insert(
-        parentNodeKey,
-        key,
-        value = key,
-        { left, right } = { left: true, right: true }
-    ) {
-        for (let node of this.preOrderTraversal()) {
-            if (node.key === parentNodeKey) {
-                const canInsertLeft = left && node.left === null;
-                const canInsertRight = right && node.right === null;
+  *preOrderTraversal(node = this.root) {
+    yield node;
+    if (node.left) yield* this.preOrderTraversal(node.left);
+    if (node.right) yield* this.preOrderTraversal(node.right);
+  }
 
-                if (!canInsertLeft && !canInsertRight) return false;
+  insert(
+    parentNodeKey,
+    key,
+    value = key,
+    { left, right } = { left: true, right: true }
+  ) {
+    for (let node of this.preOrderTraversal()) {
+      if (node.key === parentNodeKey) {
+        const canInsertLeft = left && node.left === null;
+        const canInsertRight = right && node.right === null;
 
-                if (canInsertLeft) {
-                    node.left = new BinaryTreeNode(key, value, node);
-                    return true;
-                }
+        if (!canInsertLeft && !canInsertRight) return false;
 
-                if (canInsertRight) {
-                    node.right = new BinaryTreeNode(key, value, node);
-                    return true;
-                }
-            }
+        if (canInsertLeft) {
+          node.left = new BinaryTreeNode(key, value, node);
+          return true;
         }
-        return false;
-    }
 
-    remove(key) {
-        for (let node of this.preOrderTraversal()) {
-            if (node.left.key === key) {
-                node.left = null;
-                return true;
-            }
-            if (node.right.key === key) {
-                node.right = null;
-                return true;
-            }
+        if (canInsertRight) {
+          node.right = new BinaryTreeNode(key, value, node);
+          return true;
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    find(key) {
-        for (let node of this.preOrderTraversal()) {
-            if (node.key === key) return node;
-        }
-        return undefined;
+  remove(key) {
+    for (let node of this.preOrderTraversal()) {
+      if (node.left.key === key) {
+        node.left = null;
+        return true;
+      }
+      if (node.right.key === key) {
+        node.right = null;
+        return true;
+      }
     }
+    return false;
+  }
+
+  find(key) {
+    for (let node of this.preOrderTraversal()) {
+      if (node.key === key) return node;
+    }
+    return undefined;
+  }
 }
 
 //-------------------------------------------------------------------
@@ -107,7 +107,7 @@ const boxSize = 45; // size of a single box within a display array (px)
 
 function getRandArr() {
   //used after start btn once
-  $("#start-btn").remove();
+  $(".start-btn").remove();
 
   let data = {};
 
@@ -131,32 +131,39 @@ function sorter() {
     // Creates tree for the splitting steps
     splitTree = new BinaryTree(0, [...origArr]);
     // Gets the final merged result
-    mergeSort(splitTree, [...origArr], 0,0, getEmptyArr(maxDepth)); //CHANGE TO SOMETHING ABOUT POPULATING TREE
+    mergeSort(splitTree, [...origArr], 0, 0, getEmptyArr(maxDepth)); //CHANGE TO SOMETHING ABOUT POPULATING TREE
 
     // Order of steps
     splitOrder = [...splitTree.preOrderTraversal()].map((n) => n.key);
-    
+
     // Creates tree for the merging steps
     mergeOrder = [...splitTree.postOrderTraversal()].map((n) => n.key);
 
     console.log(splitOrder);
-    console.log(mergeOrder)
+    console.log(mergeOrder);
   }
 }
 function fillGameBoard(startArray, maxDepth) {
   let rowSize = startArray.length * boxSize;
 
-  let dom = `<div class="arr-holder" id="dom-hold" style="order: 0"><div class="arr-row" id="arr-row-0-a">` + formatRow(startArray, '0') + `</div></div>`;
-  let sub = `<div class="arr-holder" id="sub-hold" style="order: ${maxDepth * 3}"><div class="arr-row" id="arr-row-0-b"></div></div>`;
+  let dom =
+    `<div class="arr-holder" id="dom-hold" style="order: 0"><div class="arr-row" id="arr-row-0-a">` +
+    formatRow(startArray, "0") +
+    `</div></div>`;
+  let sub = `<div class="arr-holder" id="sub-hold" style="order: ${
+    maxDepth * 3
+  }"><div class="arr-row" id="arr-row-0-b"></div></div>`;
   let split = ``;
   let sort = ``;
   for (i = 1; i < maxDepth + 1; i++) {
-    rowSize = Math.ceil(startArray.length/(i * 2)) * boxSize;
+    rowSize = Math.ceil(startArray.length / (i * 2)) * boxSize;
     split += `<div class="arr-holder" id="arr-holder-${i}-a" style="order: ${i}">`;
-    sort += `<div class="arr-holder" id="arr-holder-${i}-b" style="order: ${maxDepth * 2 - i}">`;
+    sort += `<div class="arr-holder" id="arr-holder-${i}-b" style="order: ${
+      maxDepth * 2 - i
+    }">`;
 
     if (i < maxDepth) {
-      for (j = 0; j < Math.pow(2,i); j++) {
+      for (j = 0; j < Math.pow(2, i); j++) {
         split += `<div class="arr-row" id="arr-row-${i}-${j}-a" style="width: ${rowSize}px;"></div>`;
         sort += `<div class="arr-row" id="arr-row-${i}-${j}-b" style="width: ${rowSize}px"></div>`;
       }
@@ -170,7 +177,7 @@ function fillGameBoard(startArray, maxDepth) {
       sort += `<div class="arr-row" id="arr-row-${maxDepth}-0-b" style="width: ${rowSize}px"></div>`;
       sort += `<div class="arr-row" id="arr-row-${maxDepth}-1-b" style="width: ${rowSize}px"></div>`;
 
-      for (j = 2; j < Math.pow(2,(i - 1)); j++) {
+      for (j = 2; j < Math.pow(2, i - 1); j++) {
         split += `<div class="arr-row" style="width: ${rowSize}px"></div>`;
         sort += `<div class="arr-row" style="width: ${rowSize}px"></div>`;
       }
@@ -180,7 +187,7 @@ function fillGameBoard(startArray, maxDepth) {
       sort += `<div class="arr-row" id="arr-row-${maxDepth}-2-b" style="width: ${rowSize}px"></div>`;
       sort += `<div class="arr-row" id="arr-row-${maxDepth}-3-b" style="width: ${rowSize}px"></div>`;
 
-      for (j = 2; j < Math.pow(2,(i - 1)); j++) {
+      for (j = 2; j < Math.pow(2, i - 1); j++) {
         split += `<div class="arr-row" style="width: ${rowSize}px"></div>`;
         sort += `<div class="arr-row" style="width: ${rowSize}px"></div>`;
       }
@@ -239,98 +246,119 @@ function merge(left, right, depth) {
   return [...arr, ...left, ...right];
 }
 
-
 // dont need to sdn stuff as post due to cookies
 function getNextRow() {
   let curNode, val;
   // Increment current step
   curStep++;
-  
+
   //Using mergeOrder
-  if (curStep >= splitOrder.length && curStep < splitOrder.length + mergeOrder.length) {
+  if (
+    curStep >= splitOrder.length &&
+    curStep < splitOrder.length + mergeOrder.length
+  ) {
     curNode = splitTree.find(mergeOrder[curStep - mergeOrder.length]);
     val = curNode.getSortedValue;
     //If Current Node is the root
-    if (curNode.key === 0)
-    {
-      $('#msg').text('Algorithm Complete!');  // Updates Message div to say "Algorithm Complete"
+    if (curNode.key === 0) {
+      $("#msg").text("Algorithm Complete!"); // Updates Message div to say "Algorithm Complete"
     }
     //Updating msg div to notify the merge
-    else {$('#msg').text('[Merging] @ Tree Row: '+ (Number(curNode.key.slice(0,1)) + 1) + 
-                              ', Tree Node: ' + (Number(curNode.key.slice(2,3)) + 1));
+    else {
+      $("#msg").text(
+        "[Merging] @ Tree Row: " +
+          (Number(curNode.key.slice(0, 1)) + 1) +
+          ", Tree Node: " +
+          (Number(curNode.key.slice(2, 3)) + 1)
+      );
     }
-    if(curNode.value.length <= 1) {
+    if (curNode.value.length <= 1) {
       return getNextRow();
     }
-  } 
+  }
   //Using splitOrder
   else if (curStep < splitOrder.length) {
     curNode = splitTree.find(splitOrder[curStep]);
     val = curNode.value;
 
     //Updating msg div to notify user a split is occurring
-    $('#msg').text('[Splitting] @ Tree Row: '+ (Number(curNode.key.slice(0,1)) + 1) + 
-                              ', Tree Node: ' + (Number(curNode.key.slice(2,3)) + 1));
-  } else { 
+    $("#msg").text(
+      "[Splitting] @ Tree Row: " +
+        (Number(curNode.key.slice(0, 1)) + 1) +
+        ", Tree Node: " +
+        (Number(curNode.key.slice(2, 3)) + 1)
+    );
+  } else {
     console.log("Error. Algorithm complete, no more steps");
     return;
   }
-  
-  $(`#arr-row-${curNode.key}-a`).html(formatRow(val,curNode.key));
-  $('#next-btn').blur();
+
+  $(`#arr-row-${curNode.key}-a`).html(formatRow(val, curNode.key));
+  $("#next-btn").blur();
 
   //changes the colour when the "next button" is pressed
-  $('#next-btn').on("click", updateColour(curNode.key));
+  $("#next-btn").on("click", updateColour(curNode.key));
 }
 
 function updateColour(val) {
   //sets intial element colour to green
-  $(`#arr-row-${val}-a`).css('background-color', "lime"); 
+  $(`#arr-row-${val}-a`).css("background-color", "lime");
   //timer set to keep element green for 1sec
-  setTimeout(function() {
+  setTimeout(function () {
     revertColour(val);
   }, 1000);
 }
 
 function revertColour(val) {
   //removes green background
-  $(`#arr-row-${val}-a`).css('background-color', "");
+  $(`#arr-row-${val}-a`).css("background-color", "");
 }
 
 function getPrevRow() {
   let curNode, val;
 
-  if(curStep == 0) {
+  if (curStep == 0) {
     return;
   }
 
   //Using mergeOrder
-  if (curStep >= splitOrder.length && curStep < splitOrder.length + mergeOrder.length) {
+  if (
+    curStep >= splitOrder.length &&
+    curStep < splitOrder.length + mergeOrder.length
+  ) {
     curNode = splitTree.find(mergeOrder[curStep - mergeOrder.length]);
     val = formatRow(curNode.value, curNode.key);
 
     //If Current Node is the root
-    if (curNode.key === 0)
-    {
-      $('#msg').text('Algorithm Complete!');  // Updates Message div to say "Algorithm Complete"
+    if (curNode.key === 0) {
+      $("#msg").text("Algorithm Complete!"); // Updates Message div to say "Algorithm Complete"
     }
     //Updating msg div to notify the merge
-    else {$('#msg').text('[Merging] @ Tree Row: '+ (Number(curNode.key.slice(0,1)) + 1) + 
-                              ', Tree Node: ' + (Number(curNode.key.slice(2,3)) + 1));
+    else {
+      $("#msg").text(
+        "[Merging] @ Tree Row: " +
+          (Number(curNode.key.slice(0, 1)) + 1) +
+          ", Tree Node: " +
+          (Number(curNode.key.slice(2, 3)) + 1)
+      );
     }
 
-    if(curNode.value.length <= 1) {
+    if (curNode.value.length <= 1) {
       curStep--;
       return getPrevRow();
     }
-  } 
+  }
   //Using splitOrder
   else if (curStep < splitOrder.length) {
     curNode = splitTree.find(splitOrder[curStep]);
     val = "";
     //Updating msg div to notify user a split is occurring
-    $('#msg').text('[Splitting] @ Tree Row: '+ (Number(curNode.key.slice(0,1)) + 1) + 
-                              ', Tree Node: ' + (Number(curNode.key.slice(2,3)) + 1));
+    $("#msg").text(
+      "[Splitting] @ Tree Row: " +
+        (Number(curNode.key.slice(0, 1)) + 1) +
+        ", Tree Node: " +
+        (Number(curNode.key.slice(2, 3)) + 1)
+    );
   } else {
     console.log("Error. Algorithm complete, no more steps");
     return;
@@ -338,20 +366,22 @@ function getPrevRow() {
 
   $(`#arr-row-${curNode.key}-a`).html(val);
   curStep--;
-  $('#prev-btn').blur();
+  $("#prev-btn").blur();
 }
 
-// Formats the displayed rows accordingly (move from index but put in game index) 
+// Formats the displayed rows accordingly (move from index but put in game index)
 function formatRow(arr, key) {
   let n;
   let html = ``;
 
   if (arr.length == 1) {
-    html += `<div class="arr arr-single" id="arr-box-${key}-${0}"exp-val=${arr[0]}><div class="num-slot">${arr[0]}</div></div>`;
+    html += `<div class="arr arr-single" id="arr-box-${key}-${0}"exp-val=${
+      arr[0]
+    }><div class="num-slot">${arr[0]}</div></div>`;
   } else {
-    for(var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
       n = arr[i];
-      if(i == 0) {
+      if (i == 0) {
         html += `<div class="arr arr-start" id="arr-box-${key}-${i}"exp-val=${n}><div class="num-slot">${n}</div></div>`;
       } else if (i + 1 == arr.length) {
         html += `<div class="arr arr-end" id="arr-box-${key}-${i}" exp-val=${n}><div class="num-slot">${n}</div></div>`;
@@ -366,20 +396,20 @@ function formatRow(arr, key) {
 
 function getEmptyArr(size) {
   let arr = [];
-  for(i = 0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     arr.push([]);
   }
   return arr;
 }
 
-function confirmQuit(){    //creates a confirmation box
-  let confirmAction = confirm("Are you sure you want to quit the game?");   //asks the user if they're sure they want to quit
-  if(confirmAction)           //if they click the yes button this returns true and redirects them to the home page
-  {
+function confirmQuit() {
+  //creates a confirmation box
+  let confirmAction = confirm("Are you sure you want to quit the game?"); //asks the user if they're sure they want to quit
+  if (confirmAction) {
+    //if they click the yes button this returns true and redirects them to the home page
     window.location = "/";
-  }
-  else                       //if the user clicks cancel they get a message to continue the game
-  {
+  } //if the user clicks cancel they get a message to continue the game
+  else {
     alert("Continue Game!");
   }
 }
