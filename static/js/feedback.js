c24f9f10bc84835f96edbcf7e603d46aee37de5e
bmendoza23 <bmendoz2@uwo.ca>
2022-02-17 16:38:39 -0500

    added audio playing functions

--- static/js/feedback.js
+++ static/js/feedback.js
@@ -24,5 +24,30 @@ function revertVis(id) {
     $(`#${id}`).css('background-color', "");
 }
 
-//Audio feedback
-function ()
+/* Audio Feedback */ 
+
+//Gameover
+function playGameOverAudio(){
+    let gameOverAudio = new Audio('../audio/game_over.wav');    //Referencing gameover sound
+    gameOverAudio.play();                                       //Playing audio
+}
+
+//Win
+function playWinAudio(){
+    let winAudio = new Audio('../audio/game_win.wav');          //Referencing gameover sound
+    winAudio.play();                                            //Playing audio
+}
+
+//Right Move
+function playRightMoveAudio(){
+    let rightMoveAudio = new Audio('../audio/right_move.wav');  //Referencing gameover sound
+    rightMoveAudio.playbackRate = 2                             //Adjusting playback rate
+    rightMoveAudio.play();                                      //Playing audio
+}
+
+//Wrong Move
+function playWrongMoveAudio(){
+    let wrongMoveAudio = new Audio('../audio/wrong_move.wav');  //Referencing gameover sound
+    wrongMoveAudio.playbackRate = 2                             //Adjusting playback rate
+    wrongMoveAudio.play();                                      //Playing audio
+}
