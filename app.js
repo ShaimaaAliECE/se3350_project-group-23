a7692a5a091bfc9fcba7578e3200ad048426871d
justinbeaudry10 <jbeaudr5@uwo.ca>
2022-03-17 00:53:01 -0400

    Test port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+var bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const path = require('path'); //Used for directory path stuff
+const path = require("path"); //Used for directory path stuff
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Sets the view engine and view path
@@ -12,35 +12,35 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 // Public client side static file
-app.use(express.static('static'));
+app.use(express.static("static"));
 
 // Routes
-var mergeSortRouter = require('./routes/mergesort_routes');
+var mergeSortRouter = require("./routes/mergesort_routes");
 
 // URLs used by routes
-app.use('/merge_sort', mergeSortRouter);
+app.use("/merge_sort", mergeSortRouter);
 
 // Base case (Home) if no matching Route
-app.use('/', (req, res, next) => {
-    console.log('-> Rendering Home Page:');
-    res.status(200).render("home", {pageTitle: "Home", tabTitle: "Home"});
-    console.log('* Rendering Home Page Success.\n');
+app.use("/", (req, res, next) => {
+  console.log("-> Rendering Home Page:");
+  res.status(200).render("home", { pageTitle: "Home", tabTitle: "Home" });
+  console.log("* Rendering Home Page Success.\n");
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    res.status(err.status || 404).json({
-      message: "No such route exists"
-    })
+app.use(function (req, res, next) {
+  res.status(err.status || 404).json({
+    message: "No such route exists",
+  });
 });
-  
+
 // error handler
-app.use(function(err, req, res, next) {
-    console.log(err);
-    res.status(err.status || 500).json({
-        message: "Error Message"
-    })
+app.use(function (err, req, res, next) {
+  console.log(err);
+  res.status(err.status || 500).json({
+    message: "Error Message",
+  });
 });
 
 // Listens on port 80
-app.listen(80);
+app.listen(process.env.PORT);
