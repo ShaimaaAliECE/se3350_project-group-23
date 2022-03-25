2660bde5410d932572e6da2fabfb3073dc361a5e
cbyce <cbyce@uwo.ca>
2022-03-25 17:45:36 -0400

    Fixed logging error

--- static/js/logging.js
+++ static/js/logging.js
@@ -6,33 +6,37 @@
 */
 
 function logGame(reason) {
-    let gameInfo, data;
-    try {
-        // If the reason is undefined change to unkown
-        if(reason == undefined) {
-            reason = "unknown";
-        }
+    let gameInfo, mistakesVal, data;
 
-        gameInfo = (window.location.pathname).slice(1).split('/'); // Gets the game type and level from the url
-        gameInfo[0] = gameInfo[0].replace('_', ' '); // Replaces the underscore with a space for correct game type
-
-        data = {
-            type: gameInfo[0],
-            level: gameInfo[1],
-            playTime: toSeconds(), //Gets the elapsed played time in seconds,
-            mistakeCount: getCounter(), // Gets the Number of mistakes made
-            logReason: reason
-        };
-        
-    } catch (err) {
-        console.log("An Error Occured While Logging The Game.");
-        console.log(err);
-    } finally {
-        $.post(`/admin/log_game`, data);
-
-        //Updates the users max level if the gam was completed
-        if(reason == 'completed') {
-            $.post(`/admin/update_max_ms`, {newMax: (gameInfo[1] + 1)});
-        }
+    // If the reason is undefined change to unkown
+    if(reason == undefined) {
+        reason = "unknown";
     }
+
+    gameInfo = (window.location.pathname).slice(1).split('/'); // Gets the game type and level from the url
+    gameInfo[0] = gameInfo[0].replace('_', ' '); // Replaces the underscore with a space for correct game type
+
+    if(gameInfo[1] != 1) {
+        mistakesVal = getCounter(); // Gets the Number of mistakes made
+    } else {
+        mistakesVal = 0;
+    }
+
+    data = {
+        type: gameInfo[0],
+        level: gameInfo[1],
+        playTime: toSeconds(), //Gets the elapsed played time in seconds,
+        mistakeCount: mistakesVal, 
+        logReason: reason
+    };
+
+    $.post(`/admin/log_game`, data);
+
+    //Updates the users max level if the gam was completed
+    if(reason == 'completed') {
+        let newMaxInt = parseInt(gameInfo[1]);
+        newMaxInt++;
+        $.post(`/admin/update_max_ms`, {newMax: newMaxInt});
+    }
+    
 }
